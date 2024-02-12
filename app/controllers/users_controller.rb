aca392c55745b627c08cb386997ffa8e7c4a9c4f
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2024-02-12 12:06:42 +0430

    create action successfully created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
 def create
 @user = User.new(user_params)
 if @user.save
-  redirect_to @user, notice:"user Was successfully created"
+  # redirect_to @user, notice:"user Was successfully created"
 else
   render :new
 end
